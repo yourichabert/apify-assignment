a9e14b9337cde09ec927bf28a3b6e4f2f45b3c2a
Youri Chabert <y.chabert@aryballe.com>
2023-07-03 13:28:28 +0200

    First implementation

--- extractProducts.js
+++ extractProducts.js
@@ -1,51 +1,36 @@
-/*
-Strategy:
-I have a recursive approach.
-I recursively split the price interval into segments and subsegments.
-This algorithm can be visualized as a tree, the nodes being the segments.
-If less than 1000 products are fetched within a segment, then this segment is a leaf (i.e. a terminal node).
-If a segment is a leaf, the products it contains are returned to the parent node.
-The parent node will then return to the grand-parent node all the products it got from its children.
-That way, all products are returned from child to parent, until the root of the tree, which returns all the products.
-With this approach, one request is done for each node.
-Since the goal is to do a minimal number of requests, we should minimize the number of nodes of the tree.
-To do that, we should partition our price intervals in the most appropriate way.
-But unfortunately, we do not know what is the distribution of products within the price range.
-If the distribution was uniform, then the best strategy would be to split each segments into subsegments of equal size.
-It's very unlikely that the distribution is uniform in practice. For instance, if the e-commerce website belongs to a supermarket,
-then it will contain a lot more products cheaper than 50,000$ than products more expensive than 50,000$.
-But if it is the website of a car or boat seller, it could be completely different.
-Since we lack information about the website, we'll assume that the distribution is uniform.
+import axios from "axios";
 
+function getProductsInfo(minPrice = 0, maxPrice = 100000) {
+    return axios.get(`https://api.ecommerce.com/products?minPrice=${minPrice}&maxPrice=${maxPrice}`)
+        .then((response) => response.data)
+        .catch((error) => console.error(`Could not fetch products of price between ${minPrice} and ${maxPrice}: ${error}`));
+}
 
-Here is the (pseudo)pseudocode of my approach:
+// Computes segments bounds given an input price interval and a count of products within that interval
+function splitInterval(lowerBound, upperBound, productsCount) {
+    const segmentCount = productsCount / 1000;
+    const intervalLength = upperBound - lowerBound;
+    return [...Array(segmentCount + 1).keys()].map(segmentIndex => segmentIndex * intervalLength / segmentCount);
+}
 
-fetchProductsFromInterval(lowerIntervalBound, upperIntervalBound):
-    intervalLength = upperIntervalBound - lowerIntervalBound
-    totalProductsCount = fetch number of products in [lowerIntervalBound, upperIntervalBound]
-    minimalNumberOfRequests = totalProductsCount/1000
-    segmentsBounds = [
-        lowerIntervalBound,
-        lowerIntervalBound + intervalLength/minimalNumberOfRequests,
-        lowerIntervalBound + 2 * intervalLength/minimalNumberOfRequests,
-        ...,
-        upperIntervalBound
-    ]
-    products = []
-    for (int i=0; i<segmentBounds.length-1; i++):
-        lowerBound=segmentsBounds[i];
-        upperBound=segmentsBounds[i+1];
-        fetchedProducts = fetch up to 1000 products in segment [lowerBound, upperBound]
-        if fetched more than 1000 products:
-            products.concat(fetchProductsFromInterval(lowerSegmentBound, upperSegmentBound))
-        else: // terminal case
-            products.concat(fetchedProducts)
-    return products
+function fetchProductsFromInterval(lowerIntervalBound, upperIntervalBound) {
+    const { total: totalInInterval } = getProductsInfo(lowerIntervalBound, upperIntervalBound);
+    const segmentsBounds = splitInterval(lowerIntervalBound, upperIntervalBound, totalInInterval);
+    let products = [];
+    let lowerSegmentBound;
+    let upperSegmentBound;
+    for (let i = 0; i < segmentsBounds.length - 1; i++) {
+        lowerSegmentBound = segmentsBounds[i];
+        upperSegmentBound = segmentsBounds[i+1];
+        const { total: totalNewProducts, products: newProducts } = getProductsInfo(lowerSegmentBound, upperSegmentBound);
+        if (totalNewProducts < 1000) { // Terminal case (segment is leaf)
+            products = [...products, ...newProducts];
+        } else {
+            products = [...products, ...fetchProductsFromInterval(lowerSegmentBound, upperSegmentBound)];
+        }
+    }
+    return products;
+}
 
-allProducts = fetchProductsFromInterval(0, 100000);
-*/
-
-// TODO: add tests
-// TODO: assess complexity/number of request from total number of products
-
-axios.get('https://api.ecommerce.com/products');
+const products = fetchProductsFromInterval(0, 100000);
+console.log('Here are all the products:', products);
--- readme.md
+++ readme.md
@@ -1,2 +1,30 @@
-Dear Apifiers,
-This is my answer to the assignment that you gave me.
+Dear Apifiers,   
+This is my answer to the assignment that you gave me.    
+
+# How to launch
+## Program
+
+## Tests
+
+# Strategy description   
+I use a recursive approach.    
+I recursively split the price interval into segments and subsegments.    
+This algorithm can be visualized as a tree, the nodes being the segments.    
+If less than 1000 products are fetched within a segment, then this segment is a leaf (i.e. a terminal node).    
+If a segment is a leaf, the products it contains are returned to the parent node.    
+The parent node will then return to the grand-parent node all the products it got from its children.    
+That way, all products are returned from child to parent, until the root of the tree, which returns all the products.    
+With this approach, one request is done for each node.    
+Since the goal is to do a minimal number of requests, we should minimize the number of nodes of the tree.    
+To do that, we should partition our price intervals in the most appropriate way.    
+But unfortunately, we do not know what is the distribution of products within the price range.    
+If the distribution was uniform, then the best strategy would be to split each segments into subsegments of equal size.    
+It's very unlikely that the distribution is uniform in practice. For instance, if the e-commerce website belongs to a supermarket.    
+then it will contain a lot more products cheaper than 50,000$ than products more expensive than 50,000$.    
+But if it is the website of a car or boat seller, it could be completely different.    
+Since we lack information about the website, we'll assume that the distribution is uniform.
+
+# TODO
+- add tests
+- assess complexity/number of request from total number of products
+- design tree in readme
